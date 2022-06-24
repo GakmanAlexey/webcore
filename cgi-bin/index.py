75ab7a38d05aa4441ec605e91133eed36d2d3651
GakmanAlexey <gakman@ya.ru>
2022-06-24 05:36:16 +0300

    Update index.py

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+import requests
+
+response = requests.get('https://api.github.com')
+
 print("Content-type: text/html")
 print()
-print("<h1>Hello world!</h1>")
+print(response.status_code)
